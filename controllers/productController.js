554f224e3da729dad27b188bea1434e2e5c7892f
shii-ctrl <johnanthonykapalaran@gmail.com>
2026-03-05 22:34:27 +0800

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,7 +11,7 @@ exports.getAllGadgets = (req, res) => {
 exports.createProducts = (req, res) => {
   const {itemName, unitPrice, quantity, supplier } = req.body;
   connection.query(
-    "INSERT INTO gadgets (itemName, unitPrice, quantity, supplier) VALUES (?, ?, ?, ?)",
+    "INSERT INTO pcs112-tindahan (itemName, unitPrice, quantity, supplier) VALUES (?, ?, ?, ?)",
     [itemName, unitPrice, quantity, supplier],
     (err, result) => {
       if (err) throw err;
@@ -22,5 +22,6 @@ exports.createProducts = (req, res) => {
     },
   );
 };
+
 
 
